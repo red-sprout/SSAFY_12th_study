409be462c968b0984bdbb0639eb64f3d36a01b80
TaeYeong KIM <52776473+TaeYeong14@users.noreply.github.com>
2024-07-17 22:04:18 +0900

    Update study_day3_boj_2563.java

--- Algorithms/TaeYeong14/1주차/study_day3_boj_2563.java
+++ Algorithms/TaeYeong14/1주차/study_day3_boj_2563.java
@@ -5,29 +5,26 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String word = reader.readLine();
-        int i = 0;
-        int result = 0;
+        int num = Integer.parseInt(reader.readLine());
+        int[][] arr = new int[100][100];
 
-        while (i < word.length()) {
-            if (word.charAt(i) == 'c' && i + 1 < word.length() && (word.charAt(i + 1) == '=' || word.charAt(i + 1) == '-')) {
-                i += 2;
-            } else if (word.charAt(i) == 'd') {
-                if (i + 1 < word.length() && word.charAt(i + 1) == '-') {
-                    i += 2;
-                } else if (i + 2 < word.length() && word.charAt(i + 1) == 'z' && word.charAt(i + 2) == '=') {
-                    i += 3;
-                } else {
-                    i += 1;
+        for (int n = 0; n < num; n++) {
+            String[] coordinates = reader.readLine().split(" ");
+            int x1 = Integer.parseInt(coordinates[0]);
+            int y1 = Integer.parseInt(coordinates[1]);
+
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    arr[x1 + i][y1 + j] = 1;
                 }
-            } else if ((word.charAt(i) == 'l' || word.charAt(i) == 'n') && i + 1 < word.length() && word.charAt(i + 1) == 'j') {
-                i += 2;
-            } else if ((word.charAt(i) == 's' || word.charAt(i) == 'z') && i + 1 < word.length() && word.charAt(i + 1) == '=') {
-                i += 2;
-            } else {
-                i += 1;
             }
-            result += 1;
+        }
+
+        int result = 0;
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                result += arr[i][j];
+            }
         }
 
         System.out.println(result);
