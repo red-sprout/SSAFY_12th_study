9d7e6dbe5966034499a46bdbed6a055da633426a
suyong <tlgmdtl1118@gmail.com>
2024-08-22 17:46:45 +0900

    [SWEA]4014/오류정수정

--- Algorithms/ehanadul/week06/SWEA_4014.java
+++ Algorithms/ehanadul/week06/SWEA_4014.java
@@ -4,82 +4,79 @@ import java.util.*;
 import java.io.*;
 
 public class SWEA_4014 {
-    // 4014. 활주로 건설
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-
-        for (int tc = 1; tc <= T; tc++) {
+        int T = Integer.parseInt(br.readLine()); 
+        
+        for (int t = 1; t <= T; t++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken());
-            int X = Integer.parseInt(st.nextToken());
-
-            int[][] map = new int[N][N];
-            int[][] transposedMap = new int[N][N];
+            int N = Integer.parseInt(st.nextToken()); 
+            int X = Integer.parseInt(st.nextToken()); 
+            
+            int[][] map = new int[N][N]; // 
             for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < N; j++) {
-                    map[i][j] = Integer.parseInt(st.nextToken());
-                    transposedMap[j][i] = map[i][j];
+                for (int j = 0; j < N; j++) 
+                    map[i][j] = Integer.parseInt(st.nextToken()); 
+            }
+            
+            int runwayCount = 0; // 가능한 활주로 수
+            
+            // 행
+            next: for (int i = 0; i < N; i++) {
+                int cnt = 1; // 연속된 높이의 개수
+                boolean down = false; // 현재 내리막인지 확인
+                
+                for (int j = 1; j < N; j++) {
+                    if (map[i][j - 1] == map[i][j]) 
+                        cnt++; // 높이가 같으면 카운트 증가
+                    else if (map[i][j - 1] + 1 == map[i][j]) { // 오르막
+                        if (down && cnt < 2 * X) continue next; // 내리막 후 경사로 필요
+                        if (!down && cnt < X) continue next; // 오르막 경사로 필요
+                        down = false; // 내리막 아님
+                        cnt = 1; // 새 구간 시작
+                    } else if (map[i][j - 1] - 1 == map[i][j]) { // 내리막
+                        if (down && cnt < X) continue next; // 내리막 후 경사로 필요
+                        down = true; // 내리막으로 설정
+                        cnt = 1; // 새 구간 시작
+                    } else {
+                        continue next; // 높이 차이가 1이 아닌 경우
+                    }
                 }
+                
+                // 마지막 구간 체크
+                if (!down || (down && cnt >= X)) 
+                    runwayCount++; // 활주로 가능
             }
-
-            int count = 0;
-
-            // 활주로 가능한지 체크 -행
-            count += countRunways(map, N, X);
-            // 활주로 가능한지 체크 - 열
-            count += countRunways(transposedMap, N, X);
-
-            System.out.println("#" + tc + " " + count);
-        }
-    }
-
-   
-
-    // 활주로 가능 여부 체크
-    private static int countRunways(int[][] map, int N, int X) {
-        int count = 0;
-
-        for (int i = 0; i < N; i++) {
-            if (isPossible(map, i, N, X)) {
-                count++;
+            
+            // 열
+            next: for (int i = 0; i < N; i++) {
+                int cnt = 1; // 연속된 높이의 개수
+                boolean down = false; // 현재 내리막인지 확인
+                
+                for (int j = 1; j < N; j++) {
+                    if (map[j - 1][i] == map[j][i]) 
+                        cnt++; // 높이가 같으면 카운트 증가
+                    else if (map[j - 1][i] + 1 == map[j][i]) { // 오르막
+                        if (down && cnt < 2 * X) continue next; // 내리막 후 경사로 필요
+                        if (!down && cnt < X) continue next; // 오르막 경사로 필요
+                        down = false; // 내리막 아님
+                        cnt = 1; // 새 구간 시작
+                    } else if (map[j - 1][i] - 1 == map[j][i]) { // 내리막
+                        if (down && cnt < X) continue next; // 내리막 후 경사로 필요
+                        down = true; // 내리막으로 설정
+                        cnt = 1; // 새 구간 시작
+                    } else {
+                        continue next; // 높이 차이가 1이 아닌 경우
+                    }
+                }
+                
+                // 마지막 구간 체크
+                if (!down || (down && cnt >= X)) 
+                    runwayCount++;
             }
+            
+            System.out.println("#" + t + " " + runwayCount);
         }
-        return count;
-    }
-
-    // 활주로 가능 여부 검사
-    private static boolean isPossible(int[][] map, int index, int N, int X) {
-        int cnt = 1;
-        boolean down = false;
-
-        for (int j = 1; j < N; j++) {
-            int currentHeight = map[index][j];
-            int previousHeight = map[index][j - 1];
-
-            if (previousHeight == currentHeight) {
-                cnt++;
-            } else if (previousHeight + 1 == currentHeight) { // 오르막
-                if (!canPlaceSlope(down, cnt, X)) return false;
-                down = false;
-                cnt = 1;
-            } else if (previousHeight - 1 == currentHeight) { // 내리막
-                if (down && cnt < X) return false;
-                down = true;
-                cnt = 1;
-            } else {
-                return false; // 높이 차이가 1이 아니면 false
-            }
-        }
-
-        return !down || (down && cnt >= X);
-    }
-
-    // 경사로 놓아도 되는지 확인
-    private static boolean canPlaceSlope(boolean down, int cnt, int X) {
-        return !(down && cnt < X) && !(cnt < X);
     }
 }
-
