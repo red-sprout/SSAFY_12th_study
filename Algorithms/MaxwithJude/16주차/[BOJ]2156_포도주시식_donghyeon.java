29fc75031f4b7eaca9800da0660c8bb5a9703b06
Maxwithjude <141596763+Maxwithjude@users.noreply.github.com>
2024-10-31 17:39:38 +0900

    [BOJ]2156_포도주시식_donghyeon.java

---
+++ Algorithms/MaxwithJude/16주차/[BOJ]2156_포도주시식_donghyeon.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main4 {
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine()); // 노드 수
+        
+        int arr [] = new int[n+1];
+        int dp [] = new int[n+1];
+        
+        for(int i = 1; i<= n; i++) {
+        	arr[i] = Integer.parseInt(br.readLine());
+        }
+        
+        dp[1] = arr[1];
+        if(n > 1) {
+        	dp[2] = arr[1]+arr[2];
+        	
+        }
+        for(int i = 3; i <= n; i++) {
+        	
+        	dp[i] = Math.max(dp[i-1], Math.max(dp[i-2]+arr[i],  dp[i-3]+arr[i-1]+arr[i]));
+        }
+        System.out.println(dp[n]);
+                            
+	}
+}
