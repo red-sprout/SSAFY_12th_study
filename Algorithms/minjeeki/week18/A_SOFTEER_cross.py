b0ea8bec1be0e54fd6069efd4a37ed969b750b2d
minjeeki <minjeeki@student.42seoul.kr>
2024-12-12 17:41:07 +0900

    [SOFTEER] 교차로 / O (시간 초과 이슈 - 비어있는 경우 값 처리 오류 있었음)

--- Algorithms/minjeeki/week18/A_SOFTEER_cross.py
+++ Algorithms/minjeeki/week18/A_SOFTEER_cross.py
@@ -11,11 +11,13 @@ for i in range(N):
 cur_t = 0
 answer = [-1] * N
 while roads[0] or roads[1] or roads[2] or roads[3]:
+    min_t = int(1e9)
     move_soon = [0] * 4
     # 현재 이동 가능 차량 찾기
     for r_idx in range(4):
         if roads[r_idx]:
             tar_t, tar_i = roads[r_idx][0]
+            min_t = min(min_t, tar_t)
             if tar_t <= cur_t:
                 move_soon[r_idx] += 1
     # 현재 모든 교차로에 차량 이동 가능 => 이동 불가
@@ -24,7 +26,7 @@ while roads[0] or roads[1] or roads[2] or roads[3]:
         break
     # 현재 모든 교차로에 이동 가능 차량 없음 => 다음 시간대 확인
     elif sum_road == 0:
-        cur_t += 1
+        cur_t = min_t
         continue
     # 교차로에 차 빼기 + 해당 차량 빠진 시간 체크 + 다음 시간대 확인
     for r_idx in range(4):
